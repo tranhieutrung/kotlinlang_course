421b5cb91ccf19d9e8fc077aef5a89520d93dcd7
Tran Hieu Trung <tranhieutrung@outlook.com>
2025-09-02 11:05:16 +0300

    Add basictypes.kt

---
+++ src/main/kotlin/basictypes.kt
@@ -0,0 +1,13 @@
+fun main() {
+    // Variable declared without initialization
+    val d: Int
+    // Variable initialized
+    d = 3
+
+    // Variable explicitly typed and initialized
+    val e: String = "hello"
+
+    // Variables can be read because they have been initialized
+    println(d)
+    println(e)
+}
