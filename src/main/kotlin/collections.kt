ced97ffa21e16df99e9e5fd3ae39f7ff127bde1f
Tran Hieu Trung <tranhieutrung@outlook.com>
2025-09-02 12:40:44 +0300

    Add collections.kt to get used to collection types.

---
+++ src/main/kotlin/collections.kt
@@ -0,0 +1,97 @@
+/*
+Lists: Ordered collections of items
+Sets: Unique unordered collections of items
+Maps: Sets of key-value pairs where keys are unique and map to only one value
+ */
+
+fun main() {
+    // Lists
+    // Read-only list
+    val readOnlyShapes = listOf("triangle", "square", "circle", "triangle")
+    println(readOnlyShapes)
+    println("The first item in the list is: ${readOnlyShapes[0]}")
+    println("The first item in the list is: ${readOnlyShapes.first()}")
+    println("The last item in the list is: ${readOnlyShapes.last()}")
+
+    println("This list has ${readOnlyShapes.count()} items")
+    println("circle" in readOnlyShapes)
+
+    // Mutable list with explicit type declaration
+    val shapes: MutableList<String> = mutableListOf("triangle", "square", "circle")
+    println(shapes)
+    // Add "pentagon" to the list
+    shapes.add("pentagon")
+    println(shapes)
+    shapes.remove("pentagon")
+    println(shapes)
+
+    //To prevent unwanted modifications, we can create a read-only view of a mutable list by assigning it to a List
+    val shapesLocked: List<String> = shapes
+    println(shapesLocked)
+
+
+    //Sets
+    // Read-only set
+    val readOnlyFruit = setOf("apple", "banana", "cherry", "cherry")
+    println(readOnlyFruit)
+
+    // Mutable set with explicit type declaration
+    val fruit: MutableSet<String> = mutableSetOf("apple", "banana", "cherry", "cherry")
+    println(fruit)
+
+    // To prevent unwanted modifications, we can create a read-only view of a mutable set by assigning it to a Set:
+    val fruitLocked: Set<String> = fruit
+    println(fruitLocked)
+
+    // As sets are unordered, we can't access an item at a particular index
+    println("This set has ${readOnlyFruit.count()} items")
+    println("banana" in readOnlyFruit)
+    fruit.add("dragonfruit")    // Add "dragonfruit" to the set
+    println(fruit)
+    fruit.remove("dragonfruit") // Remove "dragonfruit" from the set
+    println(fruit)
+
+    //Maps
+    // Every key in a map must be unique so that Kotlin can understand which value we want to get.
+    // We can have duplicate values in a map.
+
+    // Read-only map
+    val readOnlyJuiceMenu = mapOf("apple" to 100, "kiwi" to 190, "orange" to 100)
+    println(readOnlyJuiceMenu)
+
+    // Mutable map with explicit type declaration
+    val juiceMenu: MutableMap<String, Int> = mutableMapOf("apple" to 100, "kiwi" to 190, "orange" to 100)
+    println(juiceMenu)
+
+    // To prevent unwanted modifications, we can create a read-only view of a mutable map by assigning it to a Map:
+    val juiceMenuLocked: Map<String, Int> = juiceMenu
+
+    // To access a value in a map
+    println("The value of apple juice is: ${readOnlyJuiceMenu["apple"]}")
+
+    // If we try to access a key-value pair with a key that doesn't exist in a map, we see a null value:
+    println("The value of pineapple juice is: ${readOnlyJuiceMenu["pineapple"]}")
+
+    // Use the indexed access operator [] to add items to a mutable map
+    juiceMenu["coconut"] = 150 // Add key "coconut" with value 150 to the map
+    println(juiceMenu)
+
+    juiceMenu.remove("orange")    // Remove key "orange" from the map
+    println(juiceMenu)
+
+    // To get the number of items in a map
+    println("This map has ${readOnlyJuiceMenu.count()} key-value pairs")
+
+    //To check if a specific key is already included in a map
+    println(readOnlyJuiceMenu.containsKey("kiwi"))
+
+    // To obtain a collection of the keys or values of a map
+    println(readOnlyJuiceMenu.keys)
+    println(readOnlyJuiceMenu.values)
+
+    // To check that a key or value is in a map
+    println("orange" in readOnlyJuiceMenu.keys)
+    // Alternatively, we don't need to use the keys property
+    println("orange" in readOnlyJuiceMenu)
+    println(200 in readOnlyJuiceMenu.values)
+}
