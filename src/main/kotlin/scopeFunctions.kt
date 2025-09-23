cd17260749b2c8ad023a3dd9b058c49a31e3b85f
tranhieutrung <tranhieutrung@outlook.com>
2025-09-23 21:47:47 +0300

    Get used to the apply scope function.

--- src/main/kotlin/scopeFunctions.kt
+++ src/main/kotlin/scopeFunctions.kt
@@ -15,17 +15,51 @@ fun getNextAddress(): String {
     return "sebastian@jetbrains.com"
 }
 
+// 2. Apply
+// Use the apply scope function to initialize objects, like a class instance, at the time of creation rather than later on in your code. This approach makes your code easier to read and manage.
+
+class Client() {
+    var token: String? = null
+    fun connect() = println("connected!")
+    fun authenticate() = println("authenticated!")
+    fun getData(): String = "Mock data"
+}
+// Normally, we create client as an instance of the Client class before initializing its token and calling member functions in the main function:
+//val client = Client()
+//
+//fun main() {
+//    client.token = "asdf"
+//    client.connect()
+//    // connected!
+//    client.authenticate()
+//    // authenticated!
+//    client.getData()
+//}
+
+// instead of that, we use apply scope function:
+
+val client = Client().apply { // global scope
+    token = "asdf"
+    connect()
+    authenticate()
+}
+
 fun main() {
+    // 1. Let
     val address: String? = getNextAddress()
-    //the input of sendNotification function is a non-null string, need to check if the address is not null
-    // normal check:
+    // 'cause the input of sendNotification function is a non-null string, need to check if the address is not null
+    // a. normal check:
     //val confirm = if(address != null) {
         //sendNotification(address)
     //} else { null }
 
-    // OR use let scope function:
+    // b. OR use let scope function:
     val confirm = address?.let {
         // Refers to the address variable via it, using the temporary scope
         sendNotification(it)
     }
+
+    // 2. Apply
+    // only call getData function
+    client.getData()
 }
