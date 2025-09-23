9a60c2f6f12483060064fb400028f3efbd365ec1
tranhieutrung <tranhieutrung@outlook.com>
2025-09-23 22:45:08 +0300

    Add 2 exercises into scopeFunctions

--- src/main/kotlin/10_scopeFunctions.kt
+++ src/main/kotlin/10_scopeFunctions.kt
@@ -78,6 +78,39 @@ class Canvas {
     fun text(x: Int, y: Int, str: String): Unit = println("$x, $y, $str")
 }
 
+// Exercise 1:
+data class ProductInfo(val priceInDollars: Double?)
+
+class Product {
+    fun getProductInfo(): ProductInfo? {
+        return ProductInfo(100.0)
+    }
+}
+
+// Rewrite this function
+//fun Product.getPriceInEuros(): Double? {
+//    val info = getProductInfo()
+//    if (info == null) return null
+//    val price = info.priceInDollars
+//    if (price == null) return null
+//    return convertToEuros(price)
+//}
+fun Product.getPriceInEuros() = getProductInfo()?.priceInDollars?.let {
+    convertToEuros(it)
+}
+
+fun convertToEuros(dollars: Double): Double {
+    return dollars * 0.85
+}
+
+// Exercise 2
+data class User2(val id: Int, var email: String)
+
+// Use the apply scope function to update the email address and then the also scope function to print a log message
+fun updateEmail(user: User2, newEmail: String): User2 = user.apply {
+    this.email = newEmail
+}. also { println("Updating email for user with ID: ${it.id}.") }
+
 fun main() {
     // 1. Let
     val address: String? = getNextAddress()
@@ -143,6 +176,24 @@ fun main() {
         circ(160, 180, 55)
         text(50, 70, "Kotlin")
     }
+
+    // Exercise 1
+    val product = Product()
+    val priceInEuros = product.getPriceInEuros()
+
+    if (priceInEuros != null) {
+       println("Price in Euros: €$priceInEuros")
+    } else {
+        println("Price information is not available.")
+    }
+
+    // Exercise 2
+    val user = User2(1, "old_email@example.com")
+    val updatedUser = updateEmail(user, "new_email@example.com")
+    // Updating email for user with ID: 1
+
+    println("Updated User: $updatedUser")
+    // Updated User: User(id=1, email=new_email@example.com)
 }
 
 /*
