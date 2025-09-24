deb573d5a35dd2211716fc1e21c6b507d308982c
tranhieutrung <tranhieutrung@outlook.com>
2025-09-24 21:20:57 +0300

    Add the new example of lamda expression with receiver.

--- src/main/kotlin/11_LambdaExpWithReceiver.kt
+++ src/main/kotlin/11_LambdaExpWithReceiver.kt
@@ -5,11 +5,35 @@ class DrawShape {
 }
 
 // Lambda expression with receiver definition
-fun render(block: DrawShape.() -> Unit): DrawShape {
+// { receiverType.() -> returnType }
+fun render(draw: DrawShape.() -> Unit): DrawShape {
     val shape = DrawShape()
     // Use the lambda expression with receiver
-    shape.block()
+    shape.draw()
     return shape
+}
+
+
+//Example 2:
+class MenuItem(val name: String)
+
+class Menu(val name: String) {
+    val items = mutableListOf<MenuItem>()
+
+    fun item(name: String) {
+        items.add(MenuItem(name))
+    }
+}
+
+fun menu(name: String, init: Menu.() -> Unit): Menu {
+    val menu = Menu(name)
+    menu.init()
+    return menu
+}
+
+fun printMenu(menu: Menu) {
+    println("Menu: ${menu.name}")
+    menu.items.forEach { println("  Item: ${it.name}") }
 }
 
 fun main() {
@@ -17,4 +41,11 @@ fun main() {
         drawCircle()
         drawSquare()
     }
+
+    val mainMenu = menu("Main menu") {
+        item("Home")
+        item("Settings")
+        item("Exit")
+    }
+    printMenu(mainMenu)
 }
